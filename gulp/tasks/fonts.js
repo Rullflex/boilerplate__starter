023dc1ff24fd1d87faf6b3e93e283636e2b90f36
Dmitry <libafer99@gmail.com>
2022-02-01 12:43:48 +0500

    fix: gulp fonts copying

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -3,7 +3,7 @@ const ttf2woff = require('gulp-ttf2woff');
 const ttf2woff2 = require('gulp-ttf2woff2');
 
 module.exports.copyFonts = function copyFonts() {
-    return gulp.src(['src/**/*.{ttf, woff2, woff}']).pipe(gulp.dest('build/'));
+    return gulp.src(['src/**/*.{ttf,woff2,woff}']).pipe(gulp.dest('build/'));
 };
 
 module.exports.convertFonts = function convertFonts() {
